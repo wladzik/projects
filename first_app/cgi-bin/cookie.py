ee412d409badfa663ad42d61d1be5505c4a8972c
Vlad <lapato.ulad@gmail.com>
2020-03-14 07:58:36 +0100

    project created

---
+++ first_app/cgi-bin/cookie.py
@@ -0,0 +1,16 @@
+﻿#!/usr/bin/env python3
+
+import os
+import http.cookies
+
+cookie = http.cookies.SimpleCookie(os.environ.get("HTTP_COOKIE"))
+name = cookie.get("name")
+
+if name is None:
+    print("Set-cookie: name=value; expires=Wed May 18 03:33:20 2033; path=/cgi-bin/; httponly")
+    print("Content-type: text/html\n")
+    print("Cookies!!!")
+else:
+    print("Content-type: text/html\n")
+    print("Cookies:")
+    print(name.value)
